c878cf974c03099638c6a48ba83ec7528b85f49b
niranjan20rc <niranjanrc20@gmail.com>
2025-08-10 21:57:03 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 
-// Simple GET endpoint
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// GET
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+  res.json({ message: 'Hello from GET!' });
 });
+
+// POST
+app.post('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from POST!', data: req.body });
+});
+
+// PUT
+app.put('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from PUT!', data: req.body });
+});
+
+// DELETE
+app.delete('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from DELETE!' });
+});
+
+
 
 // Export the app as a Vercel serverless function
 module.exports = app;
